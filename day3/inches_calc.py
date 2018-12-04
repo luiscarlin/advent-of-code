46421a2941f1db1be3708c1be84090b90c2ce42e
Luis Carlin <luiscarlin@gmail.com>
2018-12-03 22:14:28 -0500

    day 3 done

---
+++ day3/inches_calc.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import collections
+
+def main():
+  coordinates = collections.defaultdict(int)
+
+  lines = open('input.txt').readlines()
+
+  for line in lines:
+    id, x, y, width, height = parse_claim(line)
+
+    for dx in range(width):
+      for dy in range(height):
+        coordinates[x + dx, y + dy] += 1
+
+  shared = 0
+
+  for (x,y), val in coordinates.items():
+    if val > 1:
+      shared += 1
+
+  print ('shared overlaps', shared)
+
+  for line in lines:
+    id, x, y, width, height = parse_claim(line)
+
+    no_overlaps = True
+
+    for dx in range(width):
+      for dy in range(height):
+        if coordinates[x + dx, y + dy] > 1:
+          no_overlaps = False
+
+    if no_overlaps:
+      print ('clain with no overlaps', id)
+
+
+def parse_claim(claim):
+  claim = claim.split()
+  id = claim[0]
+  x, y = claim[2].split(',')
+  x, y = int(x), int(y[:-1])
+  width, height = claim[3].split('x')
+  width, height = int(width), int(height)
+
+  return id, x, y, width, height
+
+if __name__ == '__main__':
+   main()
