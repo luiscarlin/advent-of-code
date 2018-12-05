0d2a8e00ec3997fb7764453c2f9a3ed8dad9b002
Luis Carlin <luiscarlin@gmail.com>
2018-12-05 06:41:10 -0500

    day5 part2 done

--- day5/day5.py
+++ day5/day5.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
-
 def main():
   string = open('input.txt').read()
 
   reduced = react(string)
+  print('star 1:', len(reduced))
 
-  print('star 1', reduced)
+  string = open('input.txt').read()
 
+  lengths = []
+
+  for unit in ['a', 'b', 'c', 'd']:
+    newstring = string
+    newstring = newstring.replace(unit, '')
+    newstring = newstring.replace(unit.upper(), '')
+
+    if len(newstring) is string:
+      continue
+
+    reduced = react(newstring)
+
+    lengths.append(len(reduced))
+
+  print('star 2:', min(lengths))
 
 def react(polymer):
   processed = False
@@ -26,9 +41,7 @@ def react(polymer):
         if char.islower() and next_char.isupper() or char.isupper() and next_char.islower():
           polymer = polymer[:index] + polymer[next_index + 1:]
           break
-
   return polymer
 
-
 if __name__ == '__main__':
-   main()
+  main()
