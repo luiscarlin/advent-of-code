c0aae96741cc5d4aa87026667600c62fce1fe0dc
Luis Carlin <luiscarlin@gmail.com>
2018-12-08 20:18:57 -0500

    remove ununsed test

--- day2/checksum_calc_test.py
+++ day2/checksum_calc_test.py
@@ -14,7 +14,3 @@ def test_repeated_returns_1_when_string_contains_repeated_char_three_times():
   id = 'bacaca'
   actual = checksum_calc.repeated(id, 3)
   assert actual == 1
-
-def test_process_list_returns_number_of_strings_that_contain_repated_chars_twice():
-  ids = ['aabg', 'bbas', 'acre']
-  actual = checksum_calc.repeated_in_list(ids, 2)
