51dc23c105e28a5ee64d9c2663392172ccb1edb2
Luis Carlin <luiscarlin@gmail.com>
2018-12-09 19:58:54 -0500

    clean up a bit

--- day9/day9.py
+++ day9/day9.py
@@ -5,19 +5,21 @@ from collections import defaultdict, deque
 words = open('input.txt').read().split()
 
 num_players = int(words[0])
-last_marble = int(words[6])*100
+last_marble = int(words[6])
 
 players = deque(range(1, num_players + 1))
 
 def main():
+  get_score(last_marble)
+  get_score(last_marble * 100)
+
+def get_score(num_marbes):
   scores = defaultdict(int)
 
   circle = deque()
-
-  current_marble = 0
   circle.append(0)
 
-  for num in range(1, last_marble + 1):
+  for num in range(1, num_marbes + 1):
     player_id = get_player()
 
     if num % 23 == 0:
@@ -26,18 +28,13 @@ def main():
       circle.rotate(7)
       removed = circle.pop()
 
-
       scores[player_id] += removed
       circle.rotate(-1)
-
     else:
       circle.rotate(-1)
-
       circle.append(num)
 
-    current_marble = circle[-1]
-
-  print('scores', max(scores.values()))
+  print('max score for', num_marbes, 'marbles and', num_players, 'players =', max(scores.values()))
 
 def get_player():
   global players
