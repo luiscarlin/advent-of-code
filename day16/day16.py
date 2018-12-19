0bcce291f3a4528a5f4ccbe6c5ca450f92253e93
Luis Carlin <luiscarlin@gmail.com>
2018-12-18 23:01:27 -0500

    day 16 part 1 done

--- day16/day16.py
+++ day16/day16.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+import re
+
 '''
 - registers = [0, 1, 2, 3]
 - 16 opcodes
@@ -15,7 +18,75 @@ borr => regA | regB => regC
 bori => regA | valB => regC
 setr => regA => regC
 seti => valA => regC
-gtir => if valA > regB => 1 into
-
-
+gtir => if valA > regB: 1 => regC else: 0 => regC
+gtri => if regA > valB: 1 => regC else: 0 => regC
+gtrr => if regA > regB: 1 => regC else: 0 => regC
+eqir => if valA == regB: 1 => regC else: 0 => regC
+eqri => if regA == valB: 1 => regC else: 0 => regC
+eqrr => if regA == regB: 1 => regC else: 0 => regC
 '''
+
+# Before: [3, 2, 1, 1]
+# 9 2 1 2
+# After:  [3, 2, 2, 1]
+
+def construct_fn(operation_func):
+  def func(instruction, before_reg):
+    opcode, regA, regB, regC = instruction
+
+    result = operation_func(regA, regB, before_reg)
+
+    after_reg = list(before_reg)
+    after_reg[regC] = result
+
+    return after_reg
+  return func
+
+def main():
+  operations = construct_all_operations()
+
+  lines = open('part1_input.txt').read().strip().split('\n')
+
+  number_samples = 0
+
+  for i, line in enumerate(lines):
+    if 'Before' in line:
+      before_reg = list(map(int, re.findall(r'-?\d+', line)))
+      instruction = list(map(int, re.findall(r'-?\d+', lines[i + 1])))
+      after_req = list(map(int, re.findall(r'-?\d+', lines[i + 2])))
+
+      matching = 0
+      for operation in operations.values():
+        if operation(instruction, before_reg) == after_req:
+          matching += 1
+
+      if matching >= 3:
+        number_samples += 1
+
+  print('part 1', number_samples)
+
+def construct_all_operations():
+  operations = {
+    'addr': construct_fn(lambda regA, regB, before_reg: before_reg[regA] + before_reg[regB]),
+    'addi': construct_fn(lambda regA, regB, before_reg: before_reg[regA] + regB),
+    'mulr': construct_fn(lambda regA, regB, before_reg: before_reg[regA] * before_reg[regB]),
+    'muli': construct_fn(lambda regA, regB, before_reg: before_reg[regA] * regB),
+    'banr': construct_fn(lambda regA, regB, before_reg: before_reg[regA] & before_reg[regB]),
+    'bani': construct_fn(lambda regA, regB, before_reg: before_reg[regA] & regB),
+    'borr': construct_fn(lambda regA, regB, before_reg: before_reg[regA] | before_reg[regB]),
+    'bori': construct_fn(lambda regA, regB, before_reg: before_reg[regA] | regB),
+    'setr': construct_fn(lambda regA, regB, before_reg: before_reg[regA]),
+    'seti': construct_fn(lambda regA, regB, before_reg: regA),
+    'gtir': construct_fn(lambda regA, regB, before_reg: 1 if regA > before_reg[regB] else 0),
+    'gtri': construct_fn(lambda regA, regB, before_reg: 1 if before_reg[regA] > regB else 0),
+    'gtrr': construct_fn(lambda regA, regB, before_reg: 1 if before_reg[regA] > before_reg[regB] else 0),
+    'eqir': construct_fn(lambda regA, regB, before_reg: 1 if regA == before_reg[regB] else 0),
+    'eqri': construct_fn(lambda regA, regB, before_reg: 1 if before_reg[regA] == regB else 0),
+    'eqrr': construct_fn(lambda regA, regB, before_reg: 1 if before_reg[regA] == before_reg[regB] else 0),
+  }
+  return operations
+
+
+
+if __name__ == '__main__':
+  main()
