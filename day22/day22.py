5356fe2e188405e4facd2503e30731896f4b2ca6
Luis Carlin <luiscarlin@gmail.com>
2018-12-23 15:11:45 -0500

    refactor before doing part 2

--- day22/day22.py
+++ day22/day22.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 '''
+
+part 1
+======
 - region types = rocky, narrow, wet
 - regions = X, Y (x and y >= 0)
 - input: depth and coordinate of the target
@@ -24,8 +27,21 @@
 - risk levels: 0 for rocky regions, 1 for wet regions, and 2 for narrow regions.
 
 - need to find risk level for the area where 0,0 and the target are at corners
+
+part 2
+======
+
+- 2 tools: climbing gear and torch
+
+- In rocky regions, you can use the climbing gear or the torch. You cannot use neither (you'll likely slip and fall). tools = [gear, torch]
+- In wet regions, you can use the climbing gear or neither tool. You cannot use the torch (if it gets wet, you won't have a light source). tools = [gear, neither]
+- In narrow regions, you can use the torch or neither tool. You cannot use the climbing gear (it's too bulky to fit). tools = [torch, neither]
+
+
+- You can move to an adjacent region (up, down, left, or right; never diagonally) if your currently equipped tool allows you to enter that region. Moving to an adjacent region takes one minute. (For example, if you have the torch equipped, you can move between rocky and narrow regions, but cannot enter wet regions.)
 '''
 from collections import defaultdict
+import networkx as nx
 
 def find_index(row, col, target_r, target_c, erosion_levels):
   if (row, col) is (0, 0):
@@ -52,33 +68,39 @@ def get_region_type(erosion_level):
 
   return types[idx]
 
-cave_depth = 11394
+def part1():
+  cave_depth = 11394
 
-target_r = 7
-target_c = 701
+  target_r = 7
+  target_c = 701
 
-mouth = (0, 0)
+  erosion_levels = defaultdict(int)
 
-erosion_levels = defaultdict(int)
+  risk_levels = {
+    'r': 0,
+    'w': 1,
+    'n': 2
+  }
 
-risk_levels = {
-  'r': 0,
-  'w': 1,
-  'n': 2
-}
+  risk = 0
 
-risk = 0
+  for row in range(target_r + 1):
+    for col in range(target_c + 1):
 
-for row in range(target_r + 1):
-  for col in range(target_c + 1):
+      index = find_index(row, col, target_r, target_c, erosion_levels)
 
-    index = find_index(row, col, target_r, target_c, erosion_levels)
+      erosion_level = get_erosion_level(index, cave_depth)
+      erosion_levels[(row, col)] = erosion_level
 
-    erosion_level = get_erosion_level(index, cave_depth)
-    erosion_levels[(row, col)] = erosion_level
+      region_type = get_region_type(erosion_level)
 
-    region_type = get_region_type(erosion_level)
+      risk += risk_levels[region_type]
 
-    risk += risk_levels[region_type]
+  print('part 1', risk)
 
-print('part 1', risk)
+def main():
+  part1()
+  # part2()
+
+if __name__ == '__main__':
+  main()
